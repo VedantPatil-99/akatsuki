730dc353b29ab71a6c4b7a3195fbe2911de0dfb7
VedantPatil-99 <vedantpatil9977@gmail.com>
2026-02-18 00:21:44 +0530

    refactor: remove unused imports and onMount handler from AkatsukiCanvas component

--- src/components/canvas/AkatsukiCanvas.tsx
+++ src/components/canvas/AkatsukiCanvas.tsx
@@ -1,20 +1,14 @@
 // components/canvas/AkatsukiCanvas.tsx
 "use client";
 
-import { useCallback } from "react";
-
-import { Editor, Tldraw } from "tldraw";
+import { Tldraw } from "tldraw";
 import "tldraw/tldraw.css";
 
 export default function AkatsukiCanvas() {
-  const handleMount = useCallback((editor: Editor) => {
-    console.log("Canvas Mounted! Editor ID:", editor.store.id);
-  });
   return (
     <div style={{ position: "fixed", inset: 0 }}>
       <Tldraw
         persistenceKey="akatsuki-local-dev"
-        onMount={handleMount}
         inferDarkMode
         initialState="draw"
         components={{ ZoomMenu: null, Minimap: null }}
