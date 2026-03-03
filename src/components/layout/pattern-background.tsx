ae2555d1453959448b93a495a2b113fb08127255
VedantPatil-99 <vedantpatil9977@gmail.com>
2026-03-03 17:09:19 +0530

    fix(style): pattern background horizontal line weight(opacity)

--- src/components/layout/pattern-background.tsx
+++ src/components/layout/pattern-background.tsx
@@ -23,7 +23,7 @@ export const PatternBackground = ({
         className="pointer-events-none absolute inset-0 z-0 block dark:hidden"
         style={{
           backgroundImage: `
-            repeating-linear-gradient(0deg, transparent, transparent 19px, rgba(75, 85, 99, 0.08) 19px, rgba(75, 85, 99, 0.08) 20px, transparent 20px, transparent 39px, rgba(75, 85, 99, 0.8) 39px, rgba(75, 85, 99, 0.08) 40px),
+            repeating-linear-gradient(0deg, transparent, transparent 19px, rgba(75, 85, 99, 0.08) 19px, rgba(75, 85, 99, 0.08) 20px, transparent 20px, transparent 39px, rgba(75, 85, 99, 0.08) 39px, rgba(75, 85, 99, 0.08) 40px),
             repeating-linear-gradient(90deg, transparent, transparent 19px, rgba(75, 85, 99, 0.08) 19px, rgba(75, 85, 99, 0.08) 20px, transparent 20px, transparent 39px, rgba(75, 85, 99, 0.08) 39px, rgba(75, 85, 99, 0.08) 40px),
             radial-gradient(circle at 20px 20px, rgba(55, 65, 81, 0.2) 2px, transparent 2px),
             radial-gradient(circle at 40px 40px, rgba(55, 65, 81, 0.3) 2px, transparent 2px)
